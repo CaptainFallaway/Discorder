e360b4a7cc67808985f30619f33e1dca3ee03caf
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-08-03 06:59:27 +0200

    Move getAllMessages function to fetch and reverse messages from a Discord channel

---
+++ get_all_message.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func getAllMessages(dc *DiscordClient, channelID string) ([]map[string]any, error) {
+	allMessages := make([]map[string]any, 0, 100)
+	var before string
+
+	for {
+		messages, err := dc.GetMessages(channelID, before)
+		if err != nil {
+			return nil, fmt.Errorf("error fetching messages: %w", err)
+		}
+
+		if len(messages) == 0 {
+			break
+		}
+
+		allMessages = append(allMessages, messages...)
+		before = messages[len(messages)-1]["id"].(string) // Use the last message's ID for the next request
+	}
+
+	slices.Reverse(allMessages)
+
+	return allMessages, nil
+}
